982ca70035623eb15d818631faf59afaa6df1523
赵亦凡 <178484264@qq.com>
2024-07-22 10:12:15 +0800

    版本【1.0.61】

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,4 @@
+// import type {App} from "vue";
 import './less/global.less'
 import './scss/style.scss'
 import {
@@ -21,7 +22,6 @@ import {
     bd09_To_gps84,
     gcj02_To_Bd09
 } from './utils/gps'
-import type {App} from "vue";
 
 export {
     dateFormat,
@@ -36,26 +36,25 @@ export {
     formatNumber,
     toggleDark,
     checkIdNo,
-}
-const components: any[] = [
     gps84_To_Gcj02,
     gcj02_To_Gps84,
     gps84_To_bd09,
     bd09_To_gps84,
     gcj02_To_Bd09
-]
-const install = function (app: App) {
-    // 遍历注册所有组件
-    /*
-      component.__name ts报错
-      Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
-      Type 'undefined' is not assignable to type 'string'.ts(2345)
-      解决方式一：使用// @ts-ignore
-      解决方式二：使用类型断言 尖括号语法(<string>component.__name) 或 as语法(component.__name as string)
-    */
-    components.forEach(component => app.component(component.__name as string, component))
 }
-const ToolPlugins = {
-    install,
-}
-export default ToolPlugins
+// const components: any[] = []
+// const install = function (app: App) {
+//     // 遍历注册所有组件
+//     /*
+//       component.__name ts报错
+//       Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
+//       Type 'undefined' is not assignable to type 'string'.ts(2345)
+//       解决方式一：使用// @ts-ignore
+//       解决方式二：使用类型断言 尖括号语法(<string>component.__name) 或 as语法(component.__name as string)
+//     */
+//     components.forEach(component => app.component(component.__name as string, component))
+// }
+// const ToolPlugins = {
+//     install,
+// }
+// export default ToolPlugins
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/less/global.less'
+import ToolPlugins from '../packages'
 
 const app = createApp(App)
 // window.rafTimeout = rafTimeout // 挂载到window上，全局可用，无需引入
 
-app.use(router).mount('#app')
+app.use(router).use(ToolPlugins).mount('#app')
